74fa5d2f8d2f07da56befbb44e65b988ea06e1e2
codebulb <codebulb@users.noreply.github.com>
2015-09-14 19:42:30 +0300

    JavaDoc updated

--- src/main/java/ch/codebulb/lambdaomega/package-info.java
+++ src/main/java/ch/codebulb/lambdaomega/package-info.java
@@ -1,4 +1,10 @@
 /**
- * Contains the main classes. Abstractions are placed in a sub-package.
+ * Contains the main classes. Abstractions are placed in a sub-package.<p>
+ * 
+ * The following image illustrates how the most important main classes and their abstractions are related:
+ * 
+ * <div style="clear: both; text-align: center;" class="separator">
+	<img border="0" src="../../../resources/classDiagram.png">
+    </div>
  */
 package ch.codebulb.lambdaomega;
